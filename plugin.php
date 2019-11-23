2299f12fc6c0b7af43bc7e5ca4660f2b19b9ea1a
David Kokkelink <david@datawrapper.de>
2019-11-23 22:38:04 +0100

    check emptiness instead of isset

--- plugin.php
+++ plugin.php
@@ -10,7 +10,7 @@ class DatawrapperPlugin_PublishS3 extends DatawrapperPlugin {
         $cfg = $this->getConfig();
         $plugin = $this;
 
-        if (!isset($cfg['accesskey'])) return;
+        if (empty($cfg['accesskey'])) return;
 
         $s3config = [
             'version' => '2006-03-01',
